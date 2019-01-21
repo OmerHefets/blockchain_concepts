2e64279afe2bab4d4dc539f18781508845307107
Omer Hefets <hefets.omer@gmail.com>
2019-01-21 20:10:23 +0200

    fixed priv-key bug, started address function

--- keys_address.py
+++ keys_address.py
@@ -2,6 +2,9 @@ import secrets
 import ecdsa
 import codecs
 import base58
+import sys
+import hashlib
+
 
 ECDSA_CURVE_ORDER = 'FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFEBAAEDCE6AF48A03BBFD25E8CD0364141'
 
@@ -9,25 +12,55 @@ ECDSA_CURVE_ORDER = 'FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFEBAAEDCE6AF48A03BBFD25E8CD03
 def generate_private_key():
     bits_private_key = secrets.randbits(256)
     hex_private_key = hex(bits_private_key)[2:]
-    # if the private key is bigger than the curve order, try again
+    # if the private key is bigger than the curve order, try again with recursive call
     if int(hex_private_key, 16) > int(ECDSA_CURVE_ORDER, 16):
         hex_private_key = generate_private_key()
+    hex_private_key = fix_key_length(hex_private_key, 64)
     return hex_private_key
 
 
 def generate_public_key(hex_private_key):
     bytes_private_key = codecs.decode(hex_private_key, 'hex')
-    print(bytes_private_key)
     bytes_public_key = ecdsa.SigningKey.from_string(bytes_private_key, curve=ecdsa.SECP256k1).get_verifying_key().to_string()
     hex_public_key = codecs.encode(bytes_public_key, 'hex')
     # modify pub_key display
     hex_public_key = hex(int(hex_public_key, 16))[2:]
+    hex_public_key = fix_key_length(hex_public_key, 128)
     return hex_public_key
 
 
-#def generate_address(hex_public_key):
+def generate_address(hex_public_key):
+    # encode the key for hashing
+    hex_public_key = hex_public_key.encode('utf-8')
+    # make the sha256 hash first
+    hashed_sha256_pubkey = hashlib.sha256(hex_public_key).hexdigest().encode('utf-8')
+    # make the ripemd160 hash second
+    h = hashlib.new('ripemd160')
+    h.update(hashed_sha256_pubkey)
+    pub_key_hash = h.hexdigest()
+
+
+def base58check(payload, version):
+    # add version to the payload (string concat)
+    version_payload = version + payload
+
+
+# fix the problem of too short keys in length by adding zeros
+def fix_key_length(key, requested_sized):
+    len_gap = requested_sized - len(key)
+    if len_gap == 0:
+        return key
+    elif len_gap > 0:
+        appended_zeros_string = '0' * len_gap
+        key = (appended_zeros_string + key)
+        return key
+    else:
+        sys.exit("Unidentified Error")
 
 
 priv = generate_private_key()
 print(priv)
-print(generate_public_key(priv))
+pub = generate_public_key(priv)
+print(pub)
+add = generate_address(pub)
+print(add)
--- keys_address_tests.py
+++ keys_address_tests.py
@@ -1,13 +1,17 @@
 import keys_address as ka
-
-# checking the "63 length" private key error
-# if it's since the first bit is 0, then the freq should be 1/16
+import secrets
 
 
-def freq_63_check(iter):
+"""
+checking the "63 length" private key error
+if it's since the first bit is 0, then the freq should be close to 1/16
+"""
+
+
+def freq_63_check(iterations):
     len_63 = 0
     len_64 = 0
-    for i in range(iter):
+    for i in range(iterations):
         len_priv_key = len(ka.generate_private_key())
         if len_priv_key == 64:
             len_64 += 1
@@ -17,5 +21,4 @@ def freq_63_check(iter):
     print("# of 63 length keys: {}".format(len_63))
     print("len63 / len64 ratio is: {}".format(len_63 / len_64))
 
-
-freq_63_check(10000)
+# freq_63_check(10000)
