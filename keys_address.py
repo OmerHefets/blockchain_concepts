f4ad00d9ffd1fc01466e6fd14d7efd9539151755
Omer Hefets <hefets.omer@gmail.com>
2019-01-22 20:30:54 +0200

    finished the address function

--- keys_address.py
+++ keys_address.py
@@ -11,20 +11,24 @@ ECDSA_CURVE_ORDER = 'FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFEBAAEDCE6AF48A03BBFD25E8CD03
 
 def generate_private_key():
     bits_private_key = secrets.randbits(256)
+    # cut the hex representation
     hex_private_key = hex(bits_private_key)[2:]
     # if the private key is bigger than the curve order, try again with recursive call
     if int(hex_private_key, 16) > int(ECDSA_CURVE_ORDER, 16):
         hex_private_key = generate_private_key()
+    # check key length (64), add zeros if needed
     hex_private_key = fix_key_length(hex_private_key, 64)
     return hex_private_key
 
 
 def generate_public_key(hex_private_key):
     bytes_private_key = codecs.decode(hex_private_key, 'hex')
+    # generate the pubKey from privKey with ECDSA lib
     bytes_public_key = ecdsa.SigningKey.from_string(bytes_private_key, curve=ecdsa.SECP256k1).get_verifying_key().to_string()
     hex_public_key = codecs.encode(bytes_public_key, 'hex')
     # modify pub_key display
     hex_public_key = hex(int(hex_public_key, 16))[2:]
+    # check key length (128), add zeros if needed
     hex_public_key = fix_key_length(hex_public_key, 128)
     return hex_public_key
 
@@ -38,11 +42,22 @@ def generate_address(hex_public_key):
     h = hashlib.new('ripemd160')
     h.update(hashed_sha256_pubkey)
     pub_key_hash = h.hexdigest()
+    return base58check(pub_key_hash, '00')
 
 
 def base58check(payload, version):
     # add version to the payload (string concat)
-    version_payload = version + payload
+    version_payload = (version + payload).encode('utf-8')
+    # double SHA-256
+    full_checksum = hashlib.sha256(version_payload).hexdigest().encode('utf-8')
+    full_checksum = hashlib.sha256(full_checksum).hexdigest()
+    version_payload = version_payload.decode('utf-8')
+    checksum = full_checksum[0:8]
+    version_payload += checksum
+    # encoding to bytes for base58 encoding
+    bytes_version_payload = bytes.fromhex(version_payload)
+    base58check_encoded = base58.b58encode(bytes_version_payload)
+    return base58check_encoded.decode('utf-8')
 
 
 # fix the problem of too short keys in length by adding zeros
@@ -56,6 +71,9 @@ def fix_key_length(key, requested_sized):
         return key
     else:
         sys.exit("Unidentified Error")
+
+
+base58check('3a64', '00')
 
 
 priv = generate_private_key()
